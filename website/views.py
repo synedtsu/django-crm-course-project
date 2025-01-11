806e1753db743ba43c0b87406d875c8690f1ac4f
Denys Tsiukh <emo999denchok@gmail.com>
2025-01-11 20:05:43 +0200

    Admin panel register

--- website/views.py
+++ website/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -19,7 +22,7 @@ def home(request):
             messages.success(request, "There was error logging in. Please try again.")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 # def login_user():
 #     pass
